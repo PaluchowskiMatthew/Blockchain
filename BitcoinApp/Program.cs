3884e148fd152a6b38ef93f9c1fd6629031dd6e0
Mateusz <paluchowski.matthew@gmail.com>
2017-10-20 16:48:35 +0200

    First coding session

---
+++ BitcoinApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NBitcoin;
+
+namespace BitcoinApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Key privateKey = new Key(); // generate a random private key
+
+        }
+    }
+}
+
+
+// How this project was created:
+// dotnet new console -o BitcoinApp
+// cd BitcoinApp 
+// dotnet add package NBitcoin
+// dotnet add package QBitninja.Client
+// dotnet run
